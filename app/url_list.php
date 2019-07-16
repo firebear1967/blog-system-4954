41ed147466d1a56ae443199b2983318a8d299d0f
firebear1967 <firebear@festa.ocn.ne.jp>
2019-07-17 04:53:24 +0900

    Create url_list.php
    new file

---
+++ app/url_list.php
@@ -0,0 +1,12 @@
+<?php
+$url_list = array();
+
+//$url_["/client/login.php"]="models/client/login.php";
+
+$url_list["/blog/"] = "/models/blog/index.php";
+$url_list["/blog/entry/"] = "/models/blog/entry.php";
+$url_list["/blog/category/"] = "/models/blog/category.php";
+$url_list["/blog/category_entry/"] = "/models/blog/category_entry.php";
+$url_list["/blog/setting/"] = "/models/blog/setting.php";
+
+?>
