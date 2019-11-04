4ca6201c15da53971fb985b29cef6c3c9a8fb69c
firebear1967 <firebear@festa.ocn.ne.jp>
2019-11-04 11:52:32 +0900

    update

--- config.php
+++ config.php
@@ -7,3 +7,5 @@ mb_internal_encoding("UTF-8");
 define('CONTENTS_SERVER_URL', 'http://c.blog-system-4954.localhost');
 
 define('TEMPLATE_PATH', 'C:/Dropbox/develop/blog-system-4954/app/templates');
+
+define('COPYRIGHT', '&copy;'.date("Y").' SENSE SHARE All Rights Reserved.');
