85444dd223ed3deeb232eb3004220aa42aa9fcd2
firebear1967 <firebear@festa.ocn.ne.jp>
2019-11-04 11:52:17 +0900

    update

---
+++ app/templates/template_bottom.php
@@ -0,0 +1,19 @@
+</div>
+<!-- end #content -->
+
+<!-- begin #footer -->
+<?php include(TEMPLATE_PATH."/footer.php"); ?>
+<!-- end #footer -->
+
+<!-- begin scroll to top btn -->
+<?php include(TEMPLATE_PATH."/scroll_top.php"); ?>
+<!-- end scroll to top btn -->
+</div>
+<!-- end page container -->
+
+<!-- begin scripts -->
+<?php include(TEMPLATE_PATH."/scripts.php"); ?>
+<!-- end scripts -->
+
+</body>
+</html>
